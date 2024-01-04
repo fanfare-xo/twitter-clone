0425704c4a6a11bdf3b6091a3b2fb0e78d2969cd
fanfarexo <fanfarexo@gmail.com>
2024-01-05 00:39:52 +0900

    feat: 로그인 기능 추가 및 컴포넌트 UI 디자인

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,6 +1,6 @@
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { FirebaseError } from 'firebase/app';
 import { auth } from '../firebase';
@@ -45,6 +45,13 @@ const Error = styled.span`
   text-align: center;
   font-weight: 600;
   color: tomato;
+`;
+
+const Switcher = styled.span`
+  margin-top: 20px;
+  a {
+    color: #1d96f0;
+  }
 `;
 
 function CreateAccount() {
@@ -131,6 +138,9 @@ function CreateAccount() {
         />
       </Form>
       {error !== '' ? <Error>Error Message</Error> : null}
+      <Switcher>
+        Already have an account? <Link to='/login'>Login</Link>
+      </Switcher>
     </Wrapper>
   );
 }
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,8 +1,121 @@
+import { FirebaseError } from 'firebase/app';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { auth } from '../firebase';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  height: 100%;
+  padding: 50px 0px;
+`;
+
+const Title = styled.h1`
+  font-size: 42px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+  margin-top: 50px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 50px;
+  font-size: 16px;
+  &[type='submit'] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const Error = styled.span`
+  margin-top: 15px;
+  text-align: center;
+  font-weight: 600;
+  color: tomato;
+`;
+
+const Switcher = styled.span`
+  margin-top: 20px;
+  a {
+    color: #1d96f0;
+  }
+`;
+
 function Login() {
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setError('');
+    if (isLoading || email === '' || password === '') return;
+    try {
+      setLoading(true);
+      // 파이어베이스 로그인 요청
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (e) {
+      if (e instanceof FirebaseError) setError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Wrapper>
+      <Title>Log into 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          value={email}
+          onChange={onChange}
+          name='email'
+          type='email'
+          placeholder='email'
+          required
+        />
+        <Input
+          value={password}
+          onChange={onChange}
+          name='password'
+          type='password'
+          placeholder='password'
+          required
+        />
+        <Input value={isLoading ? 'Loading...' : 'Login'} type='submit' />
+      </Form>
+      {error !== '' ? <Error>Error Message</Error> : null}
+      <Switcher>
+        Don&apos;t you have an account?{' '}
+        <Link to='/create-account'>Create account</Link>
+      </Switcher>
+    </Wrapper>
   );
 }
 
