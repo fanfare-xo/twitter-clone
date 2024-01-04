bafc882cecc4bc930191d1adbe9bb5fdb14ef617
fanfarexo <fanfarexo@gmail.com>
2024-01-04 17:49:41 +0900

    feat: Firebase 설치 및 프로젝트 연결
    - Firebase 프로젝트와 웹 프로젝트를 연결하기 위해 SDK를 설치하고 구성파일을 생성함
    - Firebase 프로젝트 ID : twitter-clone-xo

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBLCXvDUfHmisNH7TnfxCcCo1cEq1t4-3c',
+  authDomain: 'twitter-clone-xo.firebaseapp.com',
+  projectId: 'twitter-clone-xo',
+  storageBucket: 'twitter-clone-xo.appspot.com',
+  messagingSenderId: '1065014874974',
+  appId: '1:1065014874974:web:ef183699aa47022dcd75cf',
+};
+
+const app = initializeApp(firebaseConfig);
