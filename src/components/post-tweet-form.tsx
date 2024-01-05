c4e9c899a4016b0527873374714ca22edf3bb937
fanfarexo <fanfarexo@gmail.com>
2024-01-06 02:10:46 +0900

    feat: 게시글 이미지 업로드 기능 추가

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import { addDoc, collection } from 'firebase/firestore';
-import { auth, db } from '../firebase';
+import { addDoc, collection, updateDoc } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { auth, db, storage } from '../firebase';
 
 const Form = styled.form`
   display: flex;
@@ -90,12 +91,20 @@ function PostTweetForm() {
     if (!user || isLoading || tweet === '' || tweet.length > 180) return;
     try {
       setLoading(true);
-      await addDoc(collection(db, 'tweets'), {
+      const doc = await addDoc(collection(db, 'tweets'), {
         tweet,
         createdAt: Date.now(),
         userId: user.uid,
         username: user.displayName || 'Anonymous',
       });
+      if (file) {
+        const locationRef = ref(storage, `tweets/${user.uid}/${doc.id}`);
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc, {
+          photo: url,
+        });
+      }
       setTweet('');
       setFile(null);
     } catch (error) {
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBLCXvDUfHmisNH7TnfxCcCo1cEq1t4-3c',
@@ -16,3 +17,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
