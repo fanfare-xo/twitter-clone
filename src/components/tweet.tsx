b6f0c47aadd58e3b9570e5f47065763940bc2bc0
fanfarexo <fanfarexo@gmail.com>
2024-01-07 13:08:35 +0900

    feat: 게시글 삭제 기능 추가

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
+import { deleteDoc, doc } from 'firebase/firestore';
 import { ITweet } from './timeline';
+import { auth, db } from '../firebase';
 
 const Wrapper = styled.div`
   display: grid;
@@ -31,12 +33,41 @@ const Payload = styled.p`
   font-size: 18px;
 `;
 
-export default function Tweet({ tweet, photo, username }: ITweet) {
+const DeleteButton = styled.button`
+  padding: 5px 10px;
+  border: 0;
+  border-radius: 5px;
+  background-color: tomato;
+  color: white;
+  font-size: 12px;
+  font-weight: 600;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
+
+export default function Tweet({ tweet, photo, username, userId, id }: ITweet) {
+  const user = auth.currentUser;
+
+  const onDelete = async () => {
+    const ok = window.confirm('Are you sure you want to delete this tweet?');
+    if (!ok || user?.uid !== userId) return;
+    try {
+      await deleteDoc(doc(db, 'tweets', id));
+    } catch (error) {
+      window.console.log(error);
+    } finally {
+      //
+    }
+  };
+
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? (
+          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+        ) : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
